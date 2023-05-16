37200eb833da0635591004343131ec2ec8745a06
Maher hassan <mahergisanalyst@gmail.com>
2023-05-16 06:17:11 +0300

    new

--- mahermap/mahermap.py
+++ mahermap/mahermap.py
@@ -3,9 +3,14 @@ import random
 import string
 
 def generate_random_string(length=5):
-    '''Generate a random string of length 
+    
+    """_summary_
+    Generate a random string of length 
     **enter the length of the string to generate the random string**
-    '''
+
+    Returns:
+        string of length you entered
+    """    
     letters = string.ascii_lowercase + string.digits + string.ascii_uppercase + string.punctuation
     return ''.join(random.choice(letters) for i in range(length))
 
